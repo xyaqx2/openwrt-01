115d7b3db1803ab3293c3f607e8cb9a7f56ab9a0
xyaqx2 <980660618@qq.com>
2024-04-03 15:24:14 +0800

    Update 1.modify.sh

--- preset-x-wrt/1.modify.sh
+++ preset-x-wrt/1.modify.sh
@@ -21,3 +21,8 @@ modify_lax_category 'package/feeds/supply/luci-app-xray-status/Makefile'
 
 # replace geodata source
 . $(dirname $0)/../extra-files/update-geodata.sh
+
+# 修改版本为编译日期
+date_version=$(date +"%y.%m.%d")
+orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+sed -i "s/${orig_version}/R${date_version} by Haiibo/g" package/lean/default-settings/files/zzz-default-settings
