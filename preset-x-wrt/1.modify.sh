64ecd85ee4401efc02698a3ec3fe006feef48034
xyaqx2 <980660618@qq.com>
2024-04-03 19:13:22 +0800

    Update 1.modify.sh

--- preset-x-wrt/1.modify.sh
+++ preset-x-wrt/1.modify.sh
@@ -22,16 +22,4 @@ modify_lax_category 'package/feeds/supply/luci-app-xray-status/Makefile'
 # replace geodata source
 . $(dirname $0)/../extra-files/update-geodata.sh
 
-# 修改版本为编译日期
-date_version=$(date +"%y.%m.%d")
-orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-sed -i "s/${orig_version}/R${date_version} by Haiibo/g" package/lean/default-settings/files/zzz-default-settings
 
-# 取消主题默认设置
-find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
-
-# 个性签名,默认增加年月日[$(TZ=UTC-8 date "+%Y.%m.%d")]
-export Customized_Information="$(TZ=UTC-8 date "+%Y.%m.%d")"  # 个性签名,你想写啥就写啥，(填0为不作修改)
-
-# 禁用ssrplus和passwall的NaiveProxy
-export Disable_NaiveProxy="1"                # 因个别源码的分支不支持编译NaiveProxy,不小心选择了就编译错误了,为减少错误,打开这个选项后,就算选择了NaiveProxy也会把NaiveProxy干掉不进行编译的(1为启用命令,填0为不作修改)
