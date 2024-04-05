797d2cc60ff4b92b85c200c0c38c44e340f611ad
xyaqx2 <980660618@qq.com>
2024-04-05 17:03:33 +0800

    Update 1.modify.sh

--- preset-immortalwrt/1.modify.sh
+++ preset-immortalwrt/1.modify.sh
@@ -3,5 +3,8 @@
 # modify login IP
 #sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
+# modify Default Theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+
 # replace geodata source
 . $(dirname $0)/../extra-files/update-geodata.sh
